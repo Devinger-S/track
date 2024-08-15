13fd1618a185db5e140db3e5f7c82062cd1acf6a
Devinger-S <laur.sindile@gmail.com>
2024-08-15 16:49:01 +0300

    modified action function to add optimistic client first

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -9,7 +9,6 @@ import prisma from "@/lib/prisma";
 export default async function TestPage() {
 
         const session: any = await auth()
-        console.log(`here is sesion from test `, session)
 const clients = await prisma.client.findMany({
     where: {
       userId: session?.user?.id
@@ -18,25 +17,11 @@ const clients = await prisma.client.findMany({
   return (
     <>
  <section className="h-screen p-4 flex-col   flex  ">
-      <div className="">
+      
         <h2 className="text-lg font-medium mb-2">Create a new client</h2>
-        <form action={onCreateClient} className="flex flex-col sm:flex-row w-full items-center gap-4">
-          <div className="flex w-full  place-self-start ">
-
-            <Input
-              type="text"
-              name="name"
-              placeholder="Client name"
-              className="grow"
-            />
-
-            <Input type="color" name="color" placeholder="Color" className="w-12" />
-            <Input type="text" name="id" defaultValue={session.user.id } className="hidden" />
-          </div>
-          <Button className='w-full sm:w-fit' type="submit">Create</Button>
-        </form>
-      </div>
-      <OptimisticComponent clients={clients} />
+        
+      
+      <OptimisticComponent clients={clients}  session={session}/>
     </section>
     </>
   )
--- src/components/optimisticComponent.tsx
+++ src/components/optimisticComponent.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { onCreateClient } from "@/app/actions";
 import { useOptimistic } from "react"
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
-export const OptimisticComponent = ({clients}:any) => {
+export const OptimisticComponent = ({clients,session}:any) => {
 const [optimisticClients, addOptimisticClient] = useOptimistic(
     clients,
     (state, newClient) => {
@@ -11,6 +14,35 @@ const [optimisticClients, addOptimisticClient] = useOptimistic(
   );
     return (
         <>
+<form action={async formData => {
+    const color = formData.get('color')
+    const name = formData.get('name')
+    addOptimisticClient({
+        id: Math.random(),
+        name:name,
+        color:color,
+    });
+    await onCreateClient(formData)
+}} 
+className="flex flex-col sm:flex-row w-full items-center gap-4"
+>
+          <div className="flex w-full  place-self-start ">
+
+            <Input
+              type="text"
+              name="name"
+              placeholder="Client name"
+              className="grow"
+            />
+
+            <Input type="color" name="color" placeholder="Color" className="w-12" />
+            <Input type="text" name="id" defaultValue={session.user.id } className="hidden" />
+          </div>
+          <Button className='w-full sm:w-fit' type="submit">Create</Button>
+        </form>
+
+
+        
         {optimisticClients.map( (item:any) => (
             <div key={item.id}>{item.name}</div>
         ))}
